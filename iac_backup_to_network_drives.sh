1f05eff203a1292711c008ec203ed33c8fdcb48d
Jonas Souza <61324433+jonasmzsouza@users.noreply.github.com>
2024-01-18 13:00:16 -0300

    update iac_backup_to_network_drives.sh

--- iac_backup_to_network_drives.sh
+++ iac_backup_to_network_drives.sh
@@ -35,7 +35,7 @@ rm -f "$TMPDB"/*
 fi
 
 echo "Creating a copy of the database in the '$TMPDB' directory..."
-/usr/bin/mysqldump -u username -p 'password' blog > "$TMPDB"/Dump_MySQL_sitenameDB-`date +%Y%m%d%H%M%S`.sql
+/usr/bin/mysqldump -u username -p 'password' dbname > "$TMPDB"/Dump_MySQL_sitenameDB-`date +%Y%m%d%H%M%S`.sql
 
 echo "Creating the '$SHARED' directory if it doesn't exist"
 if [ ! -d "$SHARED" ] ; then
